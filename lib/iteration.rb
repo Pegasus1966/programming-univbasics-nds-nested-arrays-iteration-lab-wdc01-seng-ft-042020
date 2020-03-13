331cbd4449251b9fc33d2c48c1e8f875f2031354
Gordon Wilford <g.r.wilford@gmail.com>
2020-03-13 18:15:09 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -13,10 +13,7 @@ while row_index < src.count do
   element_index = 0
   inner_results = []
   while element_index < src[row_index].count do
-      inner_results << "I love #{src[row_index][element_index]} and #{src[row_index][element_index+1]} on my pizza"
-    element_index += 1
-  end
-  outer_results << inner_results
+      outer_results << "I love #{src[row_index][element_index]} and #{src[row_index][element_index+1]} on my pizza"
   row_index += 1
 end
   outer_results
